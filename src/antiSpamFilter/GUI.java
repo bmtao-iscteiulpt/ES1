a4fb5c9f5fbe9b64ef9eee582569aa3d92b57dce
Bruno <bmtao@iscte-iul.pt>
2017-12-05 17:43:28 +0000

    Desenvolvimento do GUI - Interface gráfica do projecto - fase 1

--- src/antiSpamFilter/GUI.java
+++ src/antiSpamFilter/GUI.java
@@ -1,7 +1,85 @@
 package antiSpamFilter;
 
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class GUI {
 
-	private String s;
+	private JFrame frame;
+	
+	private JPanel panelConfigure;
+	private JLabel labelConfigureManual;
+	private JCheckBox checkBoxConfigureManual;
+	private JLabel labelConfigureAuto;
+	private JCheckBox checkBoxConfigureAuto;
+	
+	private JPanel panelPath;
+	private JLabel labelPathRules;
+	private JCheckBox checkBoxPathRules;
+	private JLabel labelPathHam;
+	private JCheckBox checkBoxPathHam;
+	private JLabel labelPathSpam;
+	private JCheckBox checkBoxPathSpam;
+	
+	public GUI() {
+		addContentInterface();
+		startInterface();
+	}
+	
+	private void addContentInterface() {
+		frame = new JFrame("Anti-Spam Controler");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setLayout(new GridLayout(3, 0));
+		
+		panelConfigure = new JPanel();
+		panelConfigure.setLayout(new FlowLayout());
+		frame.add(panelConfigure);
+		
+		labelConfigureManual = new JLabel("Configuração Manual");
+		panelConfigure.add(labelConfigureManual);
+		
+		checkBoxConfigureManual = new JCheckBox(); 
+		panelConfigure.add(checkBoxConfigureManual);
+		
+		labelConfigureAuto = new JLabel("Configuração Automática");
+		panelConfigure.add(labelConfigureAuto);
+		
+		checkBoxConfigureAuto = new JCheckBox(); 
+		panelConfigure.add(checkBoxConfigureAuto);
+		
+		panelPath = new JPanel();
+		panelPath.setLayout(new FlowLayout());
+		frame.add(panelPath);
+		
+		labelPathRules = new JLabel("Path Rules.cf");
+		panelPath.add(labelPathRules);
+		
+		checkBoxPathRules = new JCheckBox(); 
+		panelPath.add(checkBoxPathRules);
+		
+		labelPathHam = new JLabel("Path Ham.log");
+		panelPath.add(labelPathHam);
+		
+		checkBoxPathHam = new JCheckBox(); 
+		panelPath.add(checkBoxPathHam);
+		
+		labelPathSpam = new JLabel("Path Spam.log");
+		panelPath.add(labelPathSpam);
+		
+		checkBoxPathSpam = new JCheckBox(); 
+		panelPath.add(checkBoxPathSpam);
+	}
+	
+	private void startInterface() {
+		frame.pack();
+		frame.setVisible(true);
+	}
 	
 }
---
+++ src/antiSpamFilter/Main.java
@@ -0,0 +1,9 @@
+package antiSpamFilter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GUI g = new GUI();
+	}
+
+}
